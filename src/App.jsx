838cee0264e2ce88a0f9249b6365414a4f122402
Harsh Golchha <golchhaharsh497@gmail.com>
2025-04-05 10:20:02 +0530

    Adding Comments in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Dashboard from './Components/Dashboard';
 import PrivateRoute from './Components/PrivateRoute';
 function App() {
   return (
+    // /login will render login page at /login endpoint
+    // /dashboard whenever we will navigate to dashboard /dashboard it is inside wrapped in Private Route
+    //  means it will check first if token is present or not in local storage in order to find out if user is logged in if it is then only nnavigate it to the dashboard
       <Router>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
